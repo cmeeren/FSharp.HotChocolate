bcb6704befebddc18226e4a0006c619b9fbe8af5
Christer van der Meeren <christer@infotech.no>
2024-09-23 12:11:01 +0200

    Extract ListTypeConverter and SetTypeConverter to separate file

---
+++ HotChocolate.FSharp/FSharpCollectionsInput.fs
@@ -0,0 +1,62 @@
+﻿namespace HotChocolate
+
+open System
+open System.Collections
+open HotChocolate.Utilities
+open Microsoft.FSharp.Core
+
+
+type ListTypeConverter() =
+
+    interface IChangeTypeProvider with
+
+        member this.TryCreateConverter
+            (source: Type, target: Type, root: ChangeTypeProvider, converter: byref<ChangeType>)
+            =
+            match Reflection.fastTryGetInnerIEnumerableType source, Reflection.fastTryGetInnerFSharpListType target with
+            | Some sourceElementType, Some targetElementType ->
+                match root.Invoke(sourceElementType, targetElementType) with
+                | true, innerConverter ->
+                    converter <-
+                        ChangeType(fun (value: obj) ->
+                            if isNull value then
+                                null
+                            else
+                                value :?> IEnumerable
+                                |> Seq.cast<obj>
+                                |> Seq.map innerConverter.Invoke
+                                |> Reflection.fastEnumerableCast targetElementType
+                                |> Reflection.fastListOfSeq targetElementType
+                        )
+
+                    true
+                | false, _ -> false
+            | _ -> false
+
+
+type SetTypeConverter() =
+
+    interface IChangeTypeProvider with
+
+        member this.TryCreateConverter
+            (source: Type, target: Type, root: ChangeTypeProvider, converter: byref<ChangeType>)
+            =
+            match Reflection.fastTryGetInnerIEnumerableType source, Reflection.fastTryGetInnerFSharpSetType target with
+            | Some sourceElementType, Some targetElementType ->
+                match root.Invoke(sourceElementType, targetElementType) with
+                | true, innerConverter ->
+                    converter <-
+                        ChangeType(fun (value: obj) ->
+                            if isNull value then
+                                null
+                            else
+                                value :?> IEnumerable
+                                |> Seq.cast<obj>
+                                |> Seq.map innerConverter.Invoke
+                                |> Reflection.fastEnumerableCast targetElementType
+                                |> Reflection.fastSetOfSeq targetElementType
+                        )
+
+                    true
+                | false, _ -> false
+            | _ -> false
--- HotChocolate.FSharp/Library.fs
+++ HotChocolate.FSharp/Library.fs
@@ -1,68 +1,9 @@
 ﻿namespace HotChocolate
 
 
-open System
-open System.Collections
-open HotChocolate.Utilities
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.FSharp.Core
 open HotChocolate.Execution.Configuration
-
-
-type ListTypeConverter() =
-
-    interface IChangeTypeProvider with
-
-        member this.TryCreateConverter
-            (source: Type, target: Type, root: ChangeTypeProvider, converter: byref<ChangeType>)
-            =
-            match Reflection.fastTryGetInnerIEnumerableType source, Reflection.fastTryGetInnerFSharpListType target with
-            | Some sourceElementType, Some targetElementType ->
-                match root.Invoke(sourceElementType, targetElementType) with
-                | true, innerConverter ->
-                    converter <-
-                        ChangeType(fun (value: obj) ->
-                            if isNull value then
-                                null
-                            else
-                                value :?> IEnumerable
-                                |> Seq.cast<obj>
-                                |> Seq.map innerConverter.Invoke
-                                |> Reflection.fastEnumerableCast targetElementType
-                                |> Reflection.fastListOfSeq targetElementType
-                        )
-
-                    true
-                | false, _ -> false
-            | _ -> false
-
-
-type SetTypeConverter() =
-
-    interface IChangeTypeProvider with
-
-        member this.TryCreateConverter
-            (source: Type, target: Type, root: ChangeTypeProvider, converter: byref<ChangeType>)
-            =
-            match Reflection.fastTryGetInnerIEnumerableType source, Reflection.fastTryGetInnerFSharpSetType target with
-            | Some sourceElementType, Some targetElementType ->
-                match root.Invoke(sourceElementType, targetElementType) with
-                | true, innerConverter ->
-                    converter <-
-                        ChangeType(fun (value: obj) ->
-                            if isNull value then
-                                null
-                            else
-                                value :?> IEnumerable
-                                |> Seq.cast<obj>
-                                |> Seq.map innerConverter.Invoke
-                                |> Reflection.fastEnumerableCast targetElementType
-                                |> Reflection.fastSetOfSeq targetElementType
-                        )
-
-                    true
-                | false, _ -> false
-            | _ -> false
 
 
 [<AutoOpen>]
