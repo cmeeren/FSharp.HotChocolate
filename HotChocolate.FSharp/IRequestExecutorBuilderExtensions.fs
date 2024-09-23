34d793e91adc3fccf4cf4a8739928efd50257450
Christer van der Meeren <christer@infotech.no>
2024-09-23 12:11:34 +0200

    Cleanup

--- HotChocolate.FSharp/IRequestExecutorBuilderExtensions.fs
+++ HotChocolate.FSharp/IRequestExecutorBuilderExtensions.fs
@@ -1,21 +1,17 @@
-﻿namespace HotChocolate
+﻿[<AutoOpen>]
+module HotChocolate.IRequestExecutorBuilderExtensions
 
 
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.FSharp.Core
 open HotChocolate.Execution.Configuration
 
 
-[<AutoOpen>]
-module IRequestExecutorBuilderExtensions =
+type IRequestExecutorBuilder with
 
-
-    type IRequestExecutorBuilder with
-
-        member this.AddFSharpSupport() =
-            this
-                .AddFSharpTypeConverters()
-                .AddTypeConverter<ListTypeConverter>()
-                .AddTypeConverter<SetTypeConverter>()
-                .TryAddTypeInterceptor<FSharpNullabilityTypeInterceptor>()
-                .TryAddTypeInterceptor<FSharpAsyncTypeInterceptor>()
+    member this.AddFSharpSupport() =
+        this
+            .AddFSharpTypeConverters()
+            .AddTypeConverter<ListTypeConverter>()
+            .AddTypeConverter<SetTypeConverter>()
+            .TryAddTypeInterceptor<FSharpNullabilityTypeInterceptor>()
+            .TryAddTypeInterceptor<FSharpAsyncTypeInterceptor>()
